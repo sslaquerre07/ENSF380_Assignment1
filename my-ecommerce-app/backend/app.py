7244046f9048d858fa7b14ba9662a07169c6252c
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-03-28 16:01:43 -0600

    Most functionalities working for signup page

--- my-ecommerce-app/backend/app.py
+++ my-ecommerce-app/backend/app.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
 
 #List of users
 users = [
-    {"username": "user1",
-     "password": "123",
-     "email": "user1@gmail.com"
-     },
 ]
+
+@app.route('/Login', methods=['POST'])
+def authenitcate_signup():
+    data = request.json
+    for user in users:
+        if user["username"] == data["username"]:
+            return jsonify({"message":"Username already in use"})
+    users.append(data)
+    print(users)
+    return jsonify({'message': 'User added successfully'}), 201
+    
+if __name__ == "__main__":
+    app.run(debug="True")
